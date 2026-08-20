f38e8bf33a7a2c6c8338715304fd0732a6d34078
Deepak Gupta <dgdeepakgupta76@gmail.com>
2026-08-20 21:51:42 +0530

    Time: 2 ms (99.31%), Space: 44.3 MB (63.97%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i =0,
+        j = s.length()-1;
+        while(i <j){
+            char left = s.charAt(i);
+            char right =s.charAt(j);
+            if(!Character.isLetterOrDigit(left)){
+             i=i+1;
+             continue;
+            }
+
+            if(!Character.isLetterOrDigit(right)){
+             j=j-1;
+             continue;
+            }
+            if(Character.toLowerCase(left) != Character.toLowerCase(right)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
+        
