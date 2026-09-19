49386ab943abf71f50591d2fa63a65ace78f40da
Deepak Gupta <dgdeepakgupta76@gmail.com>
2026-09-19 22:14:41 +0530

    Time: 1 ms (98.13%), Space: 42.7 MB (25.62%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,15 +1,6 @@
 class Solution {
     public int addDigits(int num) {
-        while (num >= 10) {
-            int sum = 0;
-            while (num > 0) {
-                sum += num % 10;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;
+        if (num == 0) return 0;
+        return 1 + (num - 1) % 9;
     }
 }
-
-//  here what a problem i have do this in wrongs ways because  i run while loop twice but here 1st sum denotes at num so it make easy for me 
