08b42691046a8fa6dcae15ff9271cd8fe919e1ad
Deepak Gupta <dgdeepakgupta76@gmail.com>
2026-09-25 21:31:00 +0530

    Time: 2 ms (99.4%), Space: 47.2 MB (24.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -22,3 +22,5 @@ class Solution {
 
 //  here this is using of hashmap ,, this concept are new for me but i do as soon as posible ,, same logic for using two loop but time complexity are n2 but these happen brutal force techniques 
     
+
+    //  one have also as a brutal force which have time complexity is O(n2)
